78a0145c91733fd534fda3870ff7768551dc8303
Yakubu Emmanuel <yakubuemmanuelsimpa@gmail.com>
2023-04-25 22:32:41 +0100

    update on postcss

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,6 @@ export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    // eslint-disable-next-line no-undef
     ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
   },
 };
