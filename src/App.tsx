f7ced63de40ae5d80628a48cf44b78307425eb5d
Yakubu Emmanuel <yakubuemmanuelsimpa@gmail.com>
2023-04-26 06:26:23 +0100

    update on error page

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,22 @@
 /* eslint-disable react-refresh/only-export-components */
-import { Suspense, lazy, useEffect } from "react";
+import { Suspense, lazy } from "react";
 import "./App.css";
 import {
+  Navigate,
   Route,
   createBrowserRouter,
   createRoutesFromElements,
-  useNavigate,
 } from "react-router-dom";
 import ErrorPage from "./component/ErrorPage";
 
 const Layout = lazy(() => import("./layout"));
 const Dashboard = lazy(() => import("./pages/dashboard/Dashboard"));
 
-const Home: React.FC = () => {
-  const navigate = useNavigate();
-  useEffect(() => {
-    navigate("/dashboard");
-  }, []);
-  return <p>Loading...</p>;
-};
-
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path='/' element={<Home/>} />
+      <Route path='/' element={<Navigate to='/dashboard' />} />
+      <Route path='*' element={<Navigate to='/' />} />
       <Route
         element={
           <Suspense fallback={<div>Loading...</div>}>
@@ -34,8 +27,6 @@ export const router = createBrowserRouter(
       >
         <Route path='/dashboard' element={<Dashboard />} />
       </Route>
-      <Route path='*' element={<ErrorPage />} />
     </>
   )
 );
-
